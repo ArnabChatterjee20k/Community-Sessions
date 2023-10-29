9c7abe51c8e20a71b46df0ee67b7a25aa2684d7d
arnab <arnabchatterjee.ac.2@gmail.com>
2023-10-29 20:36:21 +0530

    feat -- added create blog section

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+// int 
+// let , const, var
+let int = 12
+int = 12.5
+
+// let name = "devscoc" // "<data>" inside the quotes
+// char= "d" // "" , ''
+
+// let condition = true
+// condition = false
+
+// arrays
+let nums = [1,2,3,4]
+// first element of nums
+console.log(nums[1])
